1bf1a6a1d0877ac6a574e4a5c2d1aa26327e212a
Hoshibi <tanyshiotani@hotmail.com>
2022-03-21 14:26:20 -0300

    Criação da página Detail
    - Criação do Detail com utilização de passagem de parâmentro pelo url

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,8 +1,53 @@
-export default function Detail() {
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import fs from 'fs/promises';
+
+import { DetailComponent, Navbar } from "../../components/index"
+
+export default function Detail({ car }: any) {
   
     return (
-        <div>
-            <h1>Detail</h1>
-        </div>
+        <>
+            <Navbar />
+            <DetailComponent dataCar={car}/>
+        </>
     );
   }
+
+  async function getData() {
+    const filePath = path.join(process.cwd(), 'public', 'cars.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData.toString());
+  
+    return data;
+  }
+  
+  export const getStaticProps: GetStaticProps = async context => {
+    const { params } = context;
+  
+    const carId = params?.id;
+    const data = await getData();
+    const car = data.cars.find((car: any) => car.id == carId);
+  
+    if (!car) {
+      return { notFound: true };
+    }
+  
+    return {
+      props: {
+        car
+      }
+    };
+  };
+  
+  export const getStaticPaths: GetStaticPaths = async () => {
+    const data = await getData();
+  
+    const ids = data.cars.map((car: any) => parseInt(car.id));
+    const pathsWithParams = ids.map((id: number) => ({ params: { id: id.toString() } }));
+  
+    return {
+      paths: pathsWithParams,
+      fallback: true
+    };
+  };
